f6a3a2fe2cb6fc75e17d3bff3fb7cbe21bc42f35
abidoyinsola1 <ifeobayemi@gmail.com>
2020-08-19 16:23:34 +0100

    Add testing for the logic file

---
+++ spec/scraper-logic_spec.rb
@@ -0,0 +1,19 @@
+require '../lib/scraper-logic.rb'
+
+describe WeddingRing do
+  let(:wedding_ring) { WeddingRing.new }
+  describe '#initialize' do
+    it 'initialize url to string value' do
+      expect(wedding_ring.url).to be_a(String)
+    end
+    it 'initialize wedding_band to an empty array' do
+      expect(wedding_ring.wedding_rings).to eql([])
+    end
+  end
+  describe '#scrape' do
+    it 'assigns a value to parse_page' do
+      wedding_ring.scrape
+      expect(wedding_ring.parsed_page.nil?).to eql(false)
+    end
+  end
+end
